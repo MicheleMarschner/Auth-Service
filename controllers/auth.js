b47fa72e2b8d30940e7a7f6b3d7f375715b43a29
Michele Marschner <michele.marschner@gmail.com>
2020-09-17 12:46:40 +0200

    added passport, configurated route and controller and added joi for validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,96 +3,66 @@ const asyncHandler = require('../middleware/asyncHandler');
 const User = require('../models/User');
 //const { send } = require('process');
 
-// @desc      Register user
-// @route     POST /api/v1/auth/register
-// @access    Public
-exports.register = asyncHandler(async (req, res, next) => {
-    const { name, email, password, role } = req.body;
-  
-    // Create user
-    const user = await User.create({
-      name,
+//  @desc   Sign up
+//  @route  POST /api/v1/auth/signup
+//  @access Public
+exports.signUp = asyncHandler(async (req, res, next) => {
+  // Get request values after JOI validation
+  const { name, lastName, email, password } = req.value.body;
+  // Ensure uniqueness
+  const foundUser = await User.findOne({ 'local.email': email });
+  if (foundUser) {
+    return next(new ErrorResponse('User already exists', 401));
+  }
+  // Create new user
+  const newUser = new User({
+    method: 'local',
+    name,
+    lastName,
+    local: {
       email,
-      password
-      /*role*/
-    });
-
-    sendTokenResponse(user, 200, res);
+      password,
+    },
+  });
+  await newUser.save();
+  // Send token
+  res.status(201).json({ token: newUser.getSignedJwtToken() });
 });
 
-// @desc      Register user
-// @route     POST /api/v1/auth/register
-// @access    Public
-exports.login = asyncHandler(async (req, res, next) => {
-    const { email, password } = req.body;
-
-    // Validate email & password 
-    if (!email || !password) {
-        return next(new ErrorResponse('Please provide an email and password', 400));
-    }
-
-    // Check for user
-    const user = await User.findOne({ email }).select('+password');
-
-    if(!user) {
-        return next(new ErrorResponse('Invalid credentials', 401));
-    }
-
-    // Check if password matches
-    const isMatch = await user.matchPassword(password);
-
-    if(!isMatch) {
-        return next(new ErrorResponse('Invalid credentials', 401));
-    }
-
-    sendTokenResponse(user, 200, res);
+//  @desc   Sign in
+//  @route  POST /api/v1/auth/signin
+//  @access Public
+exports.signIn = asyncHandler(async (req, res, next) => {
+  res.status(200).send({ token: req.user.getSignedJwtToken() });
 });
 
 // @desc      Get current logged in user
 // @route     GET /api/v1/auth/me
 // @access    Private
-exports.getMe = asyncHandler(async (req, res, next) => {
-    const user = await User.findById(req.user.id);
-
-    res.status(200).json({
-        success: true,
-        data: user
-    })
+exports.user = asyncHandler(async (req, res, next) => {
+  const { id, name, lastName, status } = req.user;
+  res.status(200).send({ user: { id, name, lastName, status } });
 });
 
-// @desc      Update user details
-// @route     PUT /api/v1/auth/updatedetails
-// @access    Private
-exports.updateDetails = asyncHandler(async (req, res, next) => {
-    const fieldsToUpdate = { name: req.body.name, email: req.body.email };
-    const user = await User.findByIdAndUpdate(req.body.id, fieldsToUpdate, {
-        new: true,
-        runValidators: true
-    });
-
-    res.status(200).json({
-        success: true,
-        data: user
-    })
-});
-
-// @desc      Update password details
-// @route     PUT /api/v1/auth/updatepassword
-// @access    Private
-exports.updatePassword = asyncHandler(async (req, res, next) => {
-    const user = await User.findById(req.user.id).select('+password');
-  
-    // Check current password
-    if (!(await user.matchPassword(req.body.currentPassword))) {
-      return next(new ErrorResponse('Password is incorrect', 401));
+// 	@desc	Set status
+//	@router PATCH /api/v1/auth/set-status
+//	@access Private
+exports.setStatus = asyncHandler(async (req, res, next) => {
+  const { id: idRequest } = req.user;
+  const { status: statusOld } = req.body;
+  let user = await User.findOneAndUpdate(
+    { _id: idRequest },
+    { status: statusOld },
+    {
+      new: true,
+      runValidators: true,
     }
-  
-    user.password = req.body.newPassword;
-    await user.save();
-  
-    sendTokenResponse(user, 200, res);
+  );
+  const { _id: id, name, lastName, status } = user;
+  res.status(200).send({ user: { id, name, lastName, status } });
 });
 
+/*  
 // @desc      Forgot password
 // @route     POST /api/v1/auth/forgotpassword
 // @access    Public
@@ -112,28 +82,4 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
     })
 });
 
-
-
-
-//Get token from model create cookie and send response
-const sendTokenResponse = (user, statusCode, res) => {
-    // Create token
-    const token = user.getSignedJwtToken();
-
-    const options = {
-        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
-        httpOnly: true
-    };
-
-    if(process.env.NODE_ENV === "production") {
-        options.secure = true;
-    }
-
-    res
-        .status(statusCode)
-        .cookie('token', token, options)
-        .json({
-            success: true,
-            token
-        })
-}
+*/
